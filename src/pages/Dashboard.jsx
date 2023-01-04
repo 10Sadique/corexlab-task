74080d65f5ce69fbf11dc1150ea7d33332c8ea7a
10Sadique <jafarsjahan@gmail.com>
2023-01-04 23:38:07 +0600

    dashboard - navbar complete

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,28 @@
 import {
+    ChatBubbleOutlineRounded,
     DescriptionOutlined,
     HelpOutlineOutlined,
+    KeyboardArrowDownRounded,
     NoteAltOutlined,
+    NotificationsNoneRounded,
     PaidOutlined,
+    SearchRounded,
     SettingsOutlined,
 } from '@mui/icons-material';
 import EqualizerOutlinedIcon from '@mui/icons-material/EqualizerOutlined';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import {
+    Avatar,
+    Badge,
+    Button,
+    FormControl,
+    InputAdornment,
+    Menu,
+    MenuItem,
+    OutlinedInput,
+} from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -24,9 +38,18 @@ import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import logo from '../assets/logo-alt.svg';
 
-const drawerWidth = 240;
+const drawerWidth = 200;
 
 export default function Dashboard() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
@@ -35,15 +58,87 @@ export default function Dashboard() {
                 sx={{
                     width: `calc(100% - ${drawerWidth}px)`,
                     ml: `${drawerWidth}px`,
-                    // height: '59px',
                     backgroundColor: '#fff',
                     boxShadow: 'none',
+                    color: '#222',
                 }}
             >
                 <Toolbar>
-                    <Typography variant="h6" noWrap component="span">
-                        Permanent drawer
-                    </Typography>
+                    <Box
+                        sx={{
+                            width: '100%',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <FormControl variant="outlined">
+                            <OutlinedInput
+                                size="small"
+                                placeholder="Search.."
+                                id="input-with-icon-adornment"
+                                startAdornment={
+                                    <InputAdornment position="start">
+                                        <SearchRounded />
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                gap: '20px',
+                            }}
+                        >
+                            <ChatBubbleOutlineRounded />
+                            <Badge
+                                variant="dot"
+                                color="error"
+                                overlap="circular"
+                            >
+                                <NotificationsNoneRounded />
+                            </Badge>
+
+                            <Button
+                                id="basic-button"
+                                aria-controls={open ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
+                            >
+                                <div className="flex items-center gap-3">
+                                    <Avatar
+                                        alt="Will Smith"
+                                        src="/static/images/avatar/1.jpg"
+                                    />
+                                    <Typography variant="p">
+                                        Will Smith
+                                    </Typography>
+                                    <KeyboardArrowDownRounded />
+                                </div>
+                            </Button>
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                MenuListProps={{
+                                    'aria-labelledby': 'basic-button',
+                                }}
+                            >
+                                <MenuItem onClick={handleClose}>
+                                    Profile
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    My account
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    Logout
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+                    </Box>
                 </Toolbar>
                 <Divider />
             </AppBar>
@@ -61,7 +156,7 @@ export default function Dashboard() {
             >
                 {/* <Toolbar /> */}
 
-                <img className="w-1/2 py-[18px] pl-4" src={logo} alt="" />
+                <img className="w-1/2 py-[21px] pl-4" src={logo} alt="" />
 
                 <Divider />
                 <Typography
